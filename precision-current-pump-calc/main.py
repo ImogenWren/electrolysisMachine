0db2765253e7eea34399d0a91ee23626f28cca87
PanGalacticTech <97303986+ImogenWren@users.noreply.github.com>
2024-09-04 00:10:40 +0100

    added calculation for 20mA source

--- precision-current-pump-calc/main.py
+++ precision-current-pump-calc/main.py
@@ -17,15 +17,15 @@ k = 1000
 M = k * 1000
 
 # Set up Calculation Variables
-Vin = 27.0
+#Vin = 27.0
 
-I_out_mA = 2.0
-I_out_A = I_out_mA/1000
+#I_out_mA = 2.0
+#I_out_A = I_out_mA/1000
 
-R2 = 1.33 * M
-R3 = R2
-R4 = 100
-R5 = R4
+#R2 = 1.33 * M
+#R3 = R2
+#R4 = 100
+#R5 = R4
 
 def calc_Iout(R1, Av, Vin):
     Iout = (Vin * Av)/R1
@@ -55,7 +55,7 @@ def calc_av(R2, R4):
     return Av
 
 
-def calc_resistor(Av):
+def calc_resistor(Av, Vin, I_out_A):
     R1 = (Vin * Av) / I_out_A
     return R1
 
@@ -70,21 +70,60 @@ def scale_resistor(val):
         scale = "ohm"
     return val, scale
 
+def calc_electrolysis_machine():
+    # Set up Calculation Variables
+    Vin = 27.0
 
+    I_out_mA = 2.0
+    I_out_A = I_out_mA / 1000
 
+    R2 = 1.33 * M
+    R3 = R2
+    R4 = 100
+    R5 = R4
 
-def main():
-    #print("R1 = ")
     Av = calc_av(R2, R4)
     print(f"Av: {Av}")
-    R1 = calc_resistor(Av)
+    R1 = calc_resistor(Av, Vin, I_out_A)
     R1_print = scale_resistor(R1)
     print(f"R1: {R1_print}")
-    #print(R1[0])
+    # print(R1[0])
     print("\ncheck reverse calc\n")
     Iout = calc_Iout(R1, Av, Vin)
     print(f"Iout: {Iout} mA")
     print_Iout_calc(1, R2, R3, R4, R5, Vin)
+
+
+def calc_4to20mA_source():
+    # Set up Calculation Variables
+    Vin = 12
+
+    I_out_mA = 20.0
+    I_out_A = I_out_mA / 1000
+
+    R2 = 1 * M
+    R3 = R2
+    R4 = 10 * k
+    R5 = R4
+
+    Av = calc_av(R2, R4)
+    print(f"Av: {Av}")
+    R1 = calc_resistor(Av, Vin, I_out_A)
+    R1_print = scale_resistor(R1)
+    print(f"R1: {R1_print}")
+    # print(R1[0])
+    print("\ncheck reverse calc\n")
+    Iout = calc_Iout(R1, Av, Vin)
+    print(f"Iout: {Iout} mA")
+    print_Iout_calc(R1, R2, R3, R4, R5, Vin)
+
+
+def main():
+    #print("Calculating 2.0mA source for Electroylsis")
+    #calc_electrolysis_machine()
+    print("Calculating 20mA source for sensor circuit testing")
+    calc_4to20mA_source()
+
 
 
 # Press the green button in the gutter to run the script.
